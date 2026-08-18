71ab5cf8fe03cfe512ca21e3076b4bff2e6cf381
FELIPE GONÇALVES PACHECO <26171611@edu119.local>
2026-08-18 14:03:18 -0300

    salvando alteracao

---
+++ 02-metodos-arrays/04-find.js
@@ -0,0 +1,9 @@
+const alunos = ["Giovane","Gustavo","duraes"]
+
+const aluno = alunos.find((nome) => nome === "duraes")
+
+console.log(aluno);
+
+const aluno1 = alunos.find((nome) => nome === "felipe");
+
+console.log(aluno1)
