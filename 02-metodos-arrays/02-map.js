81d1d2a63b959828d9fb4175789dd174d75f773e
FELIPE GONÇALVES PACHECO <26171611@edu119.local>
2026-08-18 13:57:40 -0300

    salvando alteracao

---
+++ 02-metodos-arrays/02-map.js
@@ -0,0 +1,11 @@
+const numeros = [1, 2, 3, 4, 5];
+
+const dobrado = numeros.map((numeros) => numeros*2);
+
+console.log(dobrado)
+
+let nomes = ["brenda", "carlos", "felipe", ]
+
+let mensagem = nomes.map((nome) => `ola, ${nome}`)
+
+console.log(mensagem)
