c5227bf11cc6f48e4f4f5d9cb934d09a1f20cf4e
FELIPE GONÇALVES PACHECO <26171611@edu119.local>
2026-08-18 14:12:35 -0300

    salvando alteracao

---
+++ 02-metodos-arrays/05-find-index.js
@@ -0,0 +1,10 @@
+let nomes = ["joao", "julia", "larissa", "livia"];
+
+const indice = nomes.findIndex((n) => n === "larissa")
+
+console.log(indice)
+
+const indice1 = nomes.findIndex((n) => n === "matheus")
+
+console.log(indice1)
+
