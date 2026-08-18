bcf1c03e47ebac902f84723efdf8c186d25634a7
FELIPE GONÇALVES PACHECO <26171611@edu119.local>
2026-08-18 11:47:35 -0300

    salvando alteracao

---
+++ 01-funcoes/07-arrow-function.js
@@ -0,0 +1,15 @@
+function somarTradicional(a , b){
+    return a + b;
+}
+
+const somarAnonima = function (a, b){
+     return a + b;
+}
+
+const somarArrowFuctior = (a,b) =>{
+    return a + b
+}
+
+console.log(somarTradicional(56,3));
+console.log(somarAnonima(76,32));
+console.log(somarArrowFuctior(4,73));
